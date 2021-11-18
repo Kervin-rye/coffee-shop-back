424bd30d4e1b679853f5e06c13944e0d775c8402
Kervin Centeno <kervingcb@gmail.com>
2021-11-18 11:27:46 -0600

    product controller added (CRUD) && routes

---
+++ src/controllers/productController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { Product } from "../models/product";
+
+const prisma = new PrismaClient();
+
+// INSERT PRODUCT
+export async function insertProd(req: Request, res: Response) {
+    const product : Product = req.body;
+    try {
+        const insertProd = await prisma.product.create({
+            data: {
+                name: product.name,
+                price: Number(product.price)
+            },
+        });
+        console.log(insertProd)
+    } catch (error) {
+        console.log(error)
+    }
+    return res.status(200).json({result: true})
+
+}
+
+// UPDATE PRODUCT
+export async function updateProd(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+        const updateProd = await prisma.product.update({
+            where: {
+                id: +id
+            },
+            data: {
+                ...req.body,
+                price: +req.body.price
+            }
+        });
+        return res.status(200).send({ data: updateProd })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// DELETE PRODUCT
+export async function deleteProd(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+        const deleteProd = await prisma.product.delete({
+            where: {
+                id: +id
+            }
+        });
+        return res.status(200).send({ data: deleteProd })
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { deleteProd, insertProd, updateProd } from "../controllers/productController";
 
 const router = Router();
+
+// Products
+router.route("/api/newProduct").post(insertProd);
+router.route("/api/product/:id").post(updateProd);
+router.route("/api/product/:id").delete(deleteProd);
 
 export default router;
